399d9a312f67e40033a88aacf8c3621ec4b3405e
flut2 <150191812+flut2@users.noreply.github.com>
2024-05-19 06:43:49 +0200

    get rid of c#

--- srv-old/GameServer/realm/entities/GuildHallPortal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameServer.realm.entities; 
-
-internal class GuildHallPortal : StaticObject
-{
-    public GuildHallPortal(RealmManager manager, ushort objType, int? life)
-        : base(manager, objType, life, false, true, false)
-    {
-    }
-}
--- srv-old/Shared/Interfaces.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shared; 
-
-public interface ISellableItem
-{
-    ushort ItemId { get; }
-    int Price { get; }
-    int Count { get; }
-}
