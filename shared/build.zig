b3b8b2ba178a4de31762b3074f8f3ad02a3ae84b
flut2 <150191812+flut2@users.noreply.github.com>
2024-10-05 07:15:22 +0100

    fix libuv defs/sources/libs naming

--- shared/build.zig
+++ shared/build.zig
@@ -1,126 +1,132 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-const LIBUV_SOURCES = [_][]const u8{
-    "src/fs-poll.c",
-    "src/idna.c",
-    "src/inet.c",
-    "src/random.c",
-    "src/strscpy.c",
-    "src/thread-common.c",
-    "src/threadpool.c",
-    "src/timer.c",
-    "src/uv-common.c",
-    "src/uv-data-getter-setters.c",
-    "src/version.c",
-    "src/strtok.c",
+const libuv_libs = struct {
+    const windows = [_][]const u8{
+        "psapi",
+        "user32",
+        "advapi32",
+        "iphlpapi",
+        "userenv",
+        "ws2_32",
+        "dbghelp",
+        "ole32",
+        "shell32",
+    };
+
+    const linux = [_][]const u8{
+        "dl", "rt",
+    };
+
+    const apple = [_][]const u8{};
 };
 
-const LIBUV_SOURCES_WINDOWS = [_][]const u8{
-    "src/win/async.c",
-    "src/win/core.c",
-    "src/win/detect-wakeup.c",
-    "src/win/dl.c",
-    "src/win/error.c",
-    "src/win/fs.c",
-    "src/win/fs-event.c",
-    "src/win/getaddrinfo.c",
-    "src/win/getnameinfo.c",
-    "src/win/handle.c",
-    "src/win/loop-watcher.c",
-    "src/win/pipe.c",
-    "src/win/poll.c",
-    "src/win/process.c",
-    "src/win/process-stdio.c",
-    "src/win/signal.c",
-    "src/win/snprintf.c",
-    "src/win/stream.c",
-    "src/win/tcp.c",
-    "src/win/thread.c",
-    "src/win/tty.c",
-    "src/win/udp.c",
-    "src/win/util.c",
-    "src/win/winapi.c",
-    "src/win/winsock.c",
+const libuv_sources = struct {
+    const base = [_][]const u8{
+        "src/fs-poll.c",
+        "src/idna.c",
+        "src/inet.c",
+        "src/random.c",
+        "src/strscpy.c",
+        "src/thread-common.c",
+        "src/threadpool.c",
+        "src/timer.c",
+        "src/uv-common.c",
+        "src/uv-data-getter-setters.c",
+        "src/version.c",
+        "src/strtok.c",
+    };
+
+    const unix = [_][]const u8{
+        "src/unix/async.c",
+        "src/unix/core.c",
+        "src/unix/dl.c",
+        "src/unix/fs.c",
+        "src/unix/getaddrinfo.c",
+        "src/unix/getnameinfo.c",
+        "src/unix/loop-watcher.c",
+        "src/unix/loop.c",
+        "src/unix/pipe.c",
+        "src/unix/poll.c",
+        "src/unix/process.c",
+        "src/unix/random-devurandom.c",
+        "src/unix/signal.c",
+        "src/unix/stream.c",
+        "src/unix/tcp.c",
+        "src/unix/thread.c",
+        "src/unix/tty.c",
+        "src/unix/udp.c",
+        "src/unix/proctitle.c",
+    };
+
+    const windows = base ++ [_][]const u8{
+        "src/win/async.c",
+        "src/win/core.c",
+        "src/win/detect-wakeup.c",
+        "src/win/dl.c",
+        "src/win/error.c",
+        "src/win/fs.c",
+        "src/win/fs-event.c",
+        "src/win/getaddrinfo.c",
+        "src/win/getnameinfo.c",
+        "src/win/handle.c",
+        "src/win/loop-watcher.c",
+        "src/win/pipe.c",
+        "src/win/poll.c",
+        "src/win/process.c",
+        "src/win/process-stdio.c",
+        "src/win/signal.c",
+        "src/win/snprintf.c",
+        "src/win/stream.c",
+        "src/win/tcp.c",
+        "src/win/thread.c",
+        "src/win/tty.c",
+        "src/win/udp.c",
+        "src/win/util.c",
+        "src/win/winapi.c",
+        "src/win/winsock.c",
+    };
+
+    const linux = base ++ unix ++ [_][]const u8{
+        "src/unix/linux.c",
+        "src/unix/proctitle.c",
+        "src/unix/procfs-exepath.c",
+        "src/unix/random-getrandom.c",
+        "src/unix/random-sysctl-linux.c",
+    };
+
+    const apple = base ++ unix ++ [_][]const u8{
+        "src/unix/darwin-proctitle.c",
+        "src/unix/darwin.c",
+        "src/unix/fsevents.c",
+        "src/unix/kqueue.c",
+        "src/unix/proctitle.c",
+        "src/unix/bsd-ifaddrs.c",
+        "src/unix/random-getentropy.c",
+    };
 };
 
-const LIBUV_DEFINITIONS_WINDOWS = [_][]const u8{
-    "-D_WIN32",
-    "-DWIN32_LEAN_AND_MEAN",
-    "-D_WIN32_WINNT=0x0602",
-    "-D_CRT_DECLARE_NONSTDC_NAMES=0",
-};
+const libuv_definitions = struct {
+    const unix = [_][]const u8{
+        // "-D_FILE_OFFSET_BITS=64",
+        "-D_LARGEFILE_SOURCE",
+    };
 
-const LIBUV_LIBS_WINDOWS = [_][]const u8{
-    "psapi",
-    "user32",
-    "advapi32",
-    "iphlpapi",
-    "userenv",
-    "ws2_32",
-    "dbghelp",
-    "ole32",
-    "shell32",
-};
+    const windows = [_][]const u8{
+        "-D_WIN32",
+        "-DWIN32_LEAN_AND_MEAN",
+        "-D_WIN32_WINNT=0x0602",
+        "-D_CRT_DECLARE_NONSTDC_NAMES=0",
+    };
 
-const LIBUV_SOURCES_UNIX = [_][]const u8{
-    "src/unix/async.c",
-    "src/unix/core.c",
-    "src/unix/dl.c",
-    "src/unix/fs.c",
-    "src/unix/getaddrinfo.c",
-    "src/unix/getnameinfo.c",
-    "src/unix/loop-watcher.c",
-    "src/unix/loop.c",
-    "src/unix/pipe.c",
-    "src/unix/poll.c",
-    "src/unix/process.c",
-    "src/unix/random-devurandom.c",
-    "src/unix/signal.c",
-    "src/unix/stream.c",
-    "src/unix/tcp.c",
-    "src/unix/thread.c",
-    "src/unix/tty.c",
-    "src/unix/udp.c",
-    "src/unix/proctitle.c",
-};
+    const linux = unix ++ [_][]const u8{
+        "-D_GNU_SOURCE",
+    };
 
-const LIBUV_DEFINITIONS_UNIX = [_][]const u8{
-    // "-D_FILE_OFFSET_BITS=64",
-    "-D_LARGEFILE_SOURCE",
-};
-
-const LIBUV_DEFINITIONS_APPLE = [_][]const u8{
-    "-D_DARWIN_UNLIMITED_SELECT=1",
-    "-D_DARWIN_USE_64_BIT_INODE=1",
-};
-
-const LIBUV_DEFINITIONS_LINUX = [_][]const u8{
-    "-D_GNU_SOURCE",
-};
-
-const LIBUV_LIBS_LINUX = [_][]const u8{
-    "dl", "rt",
-};
-
-const LIBUV_LIBS_UNIX = [_][]const u8{};
-
-const LIBUV_SOURCES_LINUX = [_][]const u8{
-    "src/unix/linux.c",
-    "src/unix/proctitle.c",
-    "src/unix/procfs-exepath.c",
-    "src/unix/random-getrandom.c",
-    "src/unix/random-sysctl-linux.c",
-};
-
-const LIBUV_SOURCES_APPLE = [_][]const u8{
-    "src/unix/darwin-proctitle.c",
-    "src/unix/darwin.c",
-    "src/unix/fsevents.c",
-    "src/unix/kqueue.c",
-    "src/unix/proctitle.c",
-    "src/unix/bsd-ifaddrs.c",
-    "src/unix/random-getentropy.c",
+    const apple = unix ++ [_][]const u8{
+        "-D_DARWIN_UNLIMITED_SELECT=1",
+        "-D_DARWIN_USE_64_BIT_INODE=1",
+    };
 };
 
 pub fn build(b: *std.Build) !void {
@@ -151,23 +157,24 @@ pub fn build(b: *std.Build) !void {
 
     inline for (.{ libuv_dep.path("include"), libuv_dep.path("src") }) |include_path| libuv.addIncludePath(include_path);
     for (switch (builtin.os.tag) {
-        .windows => LIBUV_LIBS_WINDOWS,
-        .linux => LIBUV_LIBS_LINUX ++ LIBUV_LIBS_UNIX,
-        else => LIBUV_LIBS_UNIX,
+        .windows => libuv_libs.windows,
+        .linux => libuv_libs.linux,
+        .macos => libuv_libs.apple,
+        else => @compileError("Unsupported OS"),
     }) |lib_name| libuv.linkSystemLibrary(lib_name);
     libuv.linkLibC();
     libuv.addCSourceFiles(.{
         .root = libuv_dep.path("."),
         .files = &switch (builtin.os.tag) {
-            .windows => LIBUV_SOURCES ++ LIBUV_SOURCES_WINDOWS,
-            .linux => LIBUV_SOURCES ++ LIBUV_SOURCES_LINUX ++ LIBUV_SOURCES_UNIX,
-            .macos => LIBUV_SOURCES ++ LIBUV_SOURCES_APPLE ++ LIBUV_SOURCES_UNIX,
+            .windows => libuv_sources.windows,
+            .linux => libuv_sources.linux,
+            .macos => libuv_sources.apple,
             else => @compileError("Unsupported OS"),
         },
         .flags = &switch (builtin.os.tag) {
-            .windows => LIBUV_DEFINITIONS_WINDOWS,
-            .linux => LIBUV_DEFINITIONS_LINUX ++ LIBUV_DEFINITIONS_UNIX,
-            .macos => LIBUV_DEFINITIONS_APPLE ++ LIBUV_DEFINITIONS_UNIX,
+            .windows => libuv_definitions.windows,
+            .linux => libuv_definitions.linux,
+            .macos => libuv_definitions.apple,
             else => @compileError("Unsupported OS"),
         },
     });
